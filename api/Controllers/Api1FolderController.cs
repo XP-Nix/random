a6253c930e4d4d80fed83dbb93b41f147fca5fd1
XP <88428936+XP-Nix@users.noreply.github.com>
2022-11-09 21:21:38 +0200

    Add files via upload

---
+++ api/Controllers/Api1FolderController.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class Api1FolderController : Controller
+
+    {
+
+        public static string desc { get; set; }
+
+
+
+        // GET: Api1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Api1()
+        {
+           ViewBag.Message = "api1 message";
+
+
+            dnd();
+            
+
+            return View();
+        }
+
+
+        public ActionResult dnd()
+        {
+
+            var url = "https://www.dnd5eapi.co/api/conditions/blinded";
+
+            var webClient = new WebClient();
+
+            var response = webClient.DownloadString(url);
+
+
+
+
+
+            var desc = JsonConvert.DeserializeObject<apiString>(response);
+
+            ViewBag.response = response;
+
+            string fulldesc = "";
+
+            for (int i = 0; i < desc.desc.Length; i++)
+            {
+                fulldesc = fulldesc + " " + desc.desc[i];
+            }
+
+
+            ViewBag.desc = fulldesc;
+            
+
+            
+
+            return View();
+        }
+
+        public class apiString
+        {
+            public string[] desc
+            {
+                get;
+                set;
+            }
+        }
+        
+
+    }
+
+    
+
+
+
+
+}
---
+++ api/Controllers/ApiKeyTestController.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using static WebApplication2.Controllers.Api1FolderController;
+
+namespace WebApplication2.Controllers
+{
+    public class ApiKeyTestController : Controller
+    {
+        // GET: ApiKeyTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ApiKeyTest()
+        {
+            ViewBag.Message = "api1 message";
+
+
+            api();
+
+
+            return View();
+        }
+
+
+        public ActionResult api()
+        {
+
+            var url = "https://api.nasa.gov/planetary/apod?api_key=h1Y4gLYcsSx9vJjxheVSLHRhgdlLBgAeXmymfp6z";
+
+            var webClient = new WebClient();
+
+            var response = webClient.DownloadString(url);
+
+            var img = JObject.Parse(response);
+
+            ViewBag.response = img["url"].ToString();
+            
+
+
+
+
+
+
+
+
+
+
+
+            return View();
+        }
+
+
+    }
+}
